9cf590c3bc1848c543c8166a8a2ac597f06845d2
gary <gary.white@lodgit.com>
2022-07-19 12:53:54 +0200

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+var isSquare = function (n) {
+    return false; // fix me
+}
+
+console.log(isSquare(-1)); // false
+console.log(isSquare(0)); // true
+console.log(isSquare(3)); // false
+console.log(isSquare(4)); // true
+console.log(isSquare(25)); // true
+console.log(isSquare(26)); // false
