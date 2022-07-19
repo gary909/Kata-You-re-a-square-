e5ce43fd13a13b54c7de32e469163dd5fc8e003d
gary <gary.white@lodgit.com>
2022-07-19 13:02:37 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var isSquare = function (n) {
-    return false; // fix me
+    if (Math.sqrt(n) % 1 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 console.log(isSquare(-1)); // false
